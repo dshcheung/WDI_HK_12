ccde6ee48eceb7cfb8aaf264434b1a3736f73c62
Denis Cheung <d.s.h.cheung@gmail.com>
2016-09-28 09:36:27 +0800

    updated

--- 14-assessments/w05d3-solution.js
+++ 14-assessments/w05d3-solution.js
@@ -1,6 +1,7 @@
 // Write a function to find the multiplicative persistence of a number. You must use recursion.
 
-function multiply(numStr) {
+// Shared Function for Version 1 & 2
+var multiply = function (numStr) {
   var total = 1;
 
   for (var i = 0; i < numStr.length; i++) {
@@ -8,9 +9,10 @@ function multiply(numStr) {
   }
 
   return total;
-}
+};
 
-function multiplicativePersistenceWithTimes(num, times) {
+// Version 1
+var multiplicativePersistenceWithTimes = function (num, times) {
   var numStr = String(num);
   var newNum;
 
@@ -20,14 +22,39 @@ function multiplicativePersistenceWithTimes(num, times) {
 
   newNum = multiply(numStr); // 27
   return multiplicativePersistenceWithTimes(newNum, times + 1);
-}
+};
 
-function multiplicativePersistence(num) {
+var multiplicativePersistence = function (num) {
   return multiplicativePersistenceWithTimes(num, 0);
-}
+};
 
-multiplicativePersistence(39); //fn1
-// multiplicativePersistence(27, 1); //fn2
-// multiplicativePersistence(14, 2); //fn3
-// multiplicativePersistence(4, 3);  //fn4
-// // ---> 3
+multiplicativePersistence(39);
+// Logic
+// multiplicativePersistenceWithTimes(39, 0); //fn1
+// multiplicativePersistenceWithTimes(27, 1); //fn2
+// multiplicativePersistenceWithTimes(14, 2); //fn3
+// multiplicativePersistenceWithTimes( 4, 3); //fn4
+// result -> 3
+
+
+// Version 2
+var multiplicativePersistence2 = function (num) {
+  var numStr = String(num);
+  var newNum = multiply(numStr);
+
+  if (newNum === num) {
+    return 0;
+  } else if (num < 9) {
+    return 1;
+  } else {
+    return 1 + multiplicativePersistence2(newNum);
+  }
+};
+
+multiplicativePersistence2(39);
+// Logic
+// multiplicativePersistence2(39);    //fn1
+// 1 + multiplicativePersistence2(27) // fn2
+// 1 + multiplicativePersistence2(14) // fn3
+// 1 + multiplicativePersistence2(14) // fn3
+// result -> 3
